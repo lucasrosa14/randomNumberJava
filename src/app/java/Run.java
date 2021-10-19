bb1d0ee55f7253d426cde79958b84b4b71005f2a
lucasrosa14 <contact.lucassr@gmail.com>
2021-10-18 22:03:57 -0300

    Criação dos métodos. Chamada dos métodos. Validação do número gerado != 0.

--- src/app/java/Run.java
+++ src/app/java/Run.java
@@ -5,46 +5,67 @@ import java.util.Random;
 
 public class Run {
     static Integer entrada;
+    static Integer aleatorio;
+    static int dialogResult;
 
     public static void main(String[] args) {
 
-        Random random = new Random();
-        int aleatorio = random.nextInt(1);
-        JOptionPane.showMessageDialog(null, "Número Aleatório gerado!");
-        System.out.println(aleatorio);
+        Start();
 
-        entrada = Integer.parseInt
-                (JOptionPane.showInputDialog("Digite um número"));
-        System.out.println(entrada);
+            if (dialogResult == JOptionPane.YES_OPTION) {
 
+                GeraAleatorio();
 
-        if(aleatorio == entrada){
-            JOptionPane.showMessageDialog(null, "Parabéns, você acertou!");
-        }else if(aleatorio < entrada){
-            JOptionPane.showMessageDialog(null, "Digite um número menor!");
-            entrada = Integer.parseInt
-                    (JOptionPane.showInputDialog("Digite um número"));
-        }else if(aleatorio > entrada){
-            JOptionPane.showMessageDialog(null, "Digite um número maior!");
-            entrada = Integer.parseInt
-                    (JOptionPane.showInputDialog("Digite um número"));
-        }
+                Entrada();
 
+                Compara();
+            } else {
+                JOptionPane.showMessageDialog(null, "Você fechou o programa.");
+
+            }
 
     }
 
-//    public static int Entrada() {
-//        entrada = Integer.parseInt
-//                (JOptionPane.showInputDialog("Digite um número"));
-//        System.out.println(entrada);
-//        return entrada;
-//    }
-//
-//    public int GeraAleatorio(){
-//        Random random = new Random();
-//        int aleatorio = random.nextInt(5);
-//        System.out.println(aleatorio);
-//        return aleatorio;
-//
-//    }
+    public static int Entrada() {
+        entrada = Integer.parseInt
+                (JOptionPane.showInputDialog("Digite um número entre 1 e 5"));
+        return entrada;
+    }
+
+    public static int GeraAleatorio() {
+        Random random = new Random();
+        aleatorio = random.nextInt(5);
+        while(aleatorio == 0){
+            aleatorio = random.nextInt(5);
+            return aleatorio;
+        }
+        JOptionPane.showMessageDialog(null, "Número Aleatório gerado!");
+        return aleatorio;
+
+    }
+
+    public static void Compara() {
+        boolean isTrue = true;
+        while (isTrue) {
+            if (aleatorio != entrada) {
+                if (aleatorio < entrada) {
+                    JOptionPane.showMessageDialog(null, "Digite um número menor!");
+                    Entrada();
+                } else if (aleatorio > entrada) {
+                    JOptionPane.showMessageDialog(null, "Digite um número maior!");
+                    Entrada();
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Parabéns, você acertou!");
+                isTrue = false;
+            }
+        }
+
+    }
+
+    public static int Start() {
+        int dialogButton = JOptionPane.YES_NO_OPTION;
+        dialogResult = JOptionPane.showConfirmDialog (null, "Deseja Iniciar?","Warning",dialogButton);
+        return dialogResult;
+    }
 }
